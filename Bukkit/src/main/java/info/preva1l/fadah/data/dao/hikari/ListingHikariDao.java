14e070aa7d15d9aa3172c2dc36de38a3f4654a71
Rubén <44579213+Rubenicos@users.noreply.github.com>
2025-11-03 18:32:36 -0600

    Clean error handling

--- Bukkit/src/main/java/info/preva1l/fadah/data/dao/hikari/ListingHikariDao.java
+++ Bukkit/src/main/java/info/preva1l/fadah/data/dao/hikari/ListingHikariDao.java
@@ -16,9 +16,7 @@ import java.sql.SQLException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -81,8 +79,6 @@ public class ListingHikariDao extends SqlDao<Listing> {
         return listing;
     }
 
-    private static final Set<UUID> ERROR = new HashSet<>();
-
     @Override
     protected List<Listing> selectAll(Connection con, PreparedStatement stmt) throws SQLException {
         stmt.setLong(1, Instant.now().minus(2, ChronoUnit.DAYS).toEpochMilli());
@@ -95,14 +91,7 @@ public class ListingHikariDao extends SqlDao<Listing> {
             final UUID ownerId = UUID.fromString(result.getString("owner_id"));
             final String ownerName = result.getString("owner_name");
             final String str = result.getString("item");
-            final ItemStack itemStack;
-            try {
-                itemStack = ItemSerializer.deserialize(str);
-            } catch (Throwable t) {
-                ERROR.add(id);
-                System.out.println("Error with " + id + ": " + str);
-                continue;
-            }
+            final ItemStack itemStack = ItemSerializer.deserialize(str);
             final String temp = result.getString("category");
             final String category;
             final String currency;
@@ -120,10 +109,6 @@ public class ListingHikariDao extends SqlDao<Listing> {
             final long deletionDate = creationDate + TimeUnit.DAYS.toMillis(2);
             final boolean biddable = result.getBoolean("biddable");
             list.add(new CurrentListing(id, ownerId, ownerName, itemStack, category, currency, price, tax, creationDate, deletionDate, biddable, List.of()));
-        }
-        System.out.println("Total errors = " + ERROR.size());
-        for (UUID uuid : ERROR) {
-            System.out.println(" - " + uuid);
         }
         return list;
     }
